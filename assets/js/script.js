2e268e9891e750e2a4768638f6b5fdd980b787e9
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-22 12:44:31 +0800

    Add variables to js code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// variables
+
+const numBtns = document.querySelectorAll('[data-number]');
+const operantBtns = document.querySelectorAll('[data-operation]');
+const deleteBtn = document.querySelector('[data-delete]');
+const resetBtn = document.querySelector('[data-reset]');
+const equalBtn = document.querySelector('[data-equal]');
