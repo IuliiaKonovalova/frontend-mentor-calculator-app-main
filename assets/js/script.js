156958deb440a6a7ef7325abedf26730bffcf173
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-22 19:10:08 +0800

    Add displaying value in the calc screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,14 @@
-// variables
+const calcDisplay = document.getElementById('calc_display');
+const btnsContainer = document.getElementById('btns__all');
+const btns = document.querySelectorAll('button');
 
-const numBtns = document.querySelectorAll('[data-number]');
-const operantBtns = document.querySelectorAll('[data-operation]');
-const deleteBtn = document.querySelector('[data-delete]');
-const resetBtn = document.querySelector('[data-reset]');
-const equalBtn = document.querySelector('[data-equal]');
-const inputOutput = document.querySelector('[data-input-output]');
-
-// const calculator = new Calculator(inputOutput)
-
-/**
- * Loop though each number btn,
- * listens for a click,
- * add numbers and update the display
- */
-numBtns.forEach(btn => {
+btns.forEach(btn => {
   btn.addEventListener('click', () => {
-    return
+    const btnValue = btn.textContent
+    console.log(btnValue)
+    if () {
+
+    }
+    calcDisplay.value = btnValue
   })
 })
-
-/**
- * Loop though each operator btn,
- * listens for a click,
- * add operation and update the display
- */
-operantBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    calculator.chooseOperation(btn.innerText)
-    calculator.updateDisplay()
-  })
-})
-
-equalBtn.addEventListener('click', button => {
-  calculator.compute()
-  calculator.updateDisplay()
-})
-
-resetBtn.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
-})
-
-deleteBtn.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
-})
-
-inputOutput
