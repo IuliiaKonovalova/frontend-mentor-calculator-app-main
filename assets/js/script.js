5a8d7ee066873b5f1b935054740936747e8a0921
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-22 13:12:52 +0800

    Add structure for the class calculator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+class Calculator {
+  constructor(inputOutput) {
+    this.inputOutput
+    this.clear()
+  }
+
+  reset() {
+
+  }
+
+
+  delete() {
+    this.inputOutput = ''
+    this.operation = undefined
+  }
+
+  addNumber(number) {
+
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+
+  }
+}
+
 // variables
 
 const numBtns = document.querySelectorAll('[data-number]');
@@ -5,3 +38,8 @@ const operantBtns = document.querySelectorAll('[data-operation]');
 const deleteBtn = document.querySelector('[data-delete]');
 const resetBtn = document.querySelector('[data-reset]');
 const equalBtn = document.querySelector('[data-equal]');
+const inputOutput = document.querySelector('[data-input-output]');
+
+module.exports = {
+  Calculator
+};
