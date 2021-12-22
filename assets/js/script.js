bd68100e3f310e35c32564e67aa917c6744cf16e
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-22 19:35:31 +0800

    Add data types to number btns

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,25 @@ const btns = document.querySelectorAll('button');
 btns.forEach(btn => {
   btn.addEventListener('click', () => {
     const btnValue = btn.textContent
-    console.log(btnValue)
+    // console.log(btnValue)
     let calcDisplayValue
-    if (calcDisplay.value === '') {
-      calcDisplay.value = btnValue
-      calcDisplayValue = btnValue
-    } else if (calcDisplay.value === '0') {
-      calcDisplay.value = btnValue
-    } else {
-      calcDisplay.value = calcDisplay.value + btnValue
-      calcDisplayValue = calcDisplay.value
-      console.log(calcDisplayValue)
-    }
+    const {
+      type
+    } = btn.dataset
 
+    if (type === 'number') {
+      if (calcDisplay.value === '') {
+        calcDisplay.value = btnValue
+        calcDisplayValue = btnValue
+      } else if (calcDisplay.value === '0') {
+        calcDisplay.value = btnValue
+      } else {
+        calcDisplay.value = calcDisplay.value + btnValue
+        calcDisplayValue = calcDisplay.value
+        console.log(calcDisplayValue)
+      }
+    } else {
+      console.log('fuck')
+    }
   })
 })
