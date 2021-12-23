084027797f5fd233e260df0a9cfe6919be0f2c40
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-23 14:03:12 +0800

    Add event listeners for keydown events

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,5 +39,58 @@ document.addEventListener('DOMContentLoaded', function () {
     calculator.updateDisplay()
   })
 
-
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      calculator.compute()
+      calculator.updateDisplay()
+    } else if (event.key === 'Backspace') {
+      calculator.delete()
+      calculator.updateDisplay()
+    } else if (event.key === '+') {
+      calculator.chooseOperation('+')
+      calculator.updateDisplay()
+    } else if (event.key === '-') {
+      calculator.chooseOperation('-')
+      calculator.updateDisplay()
+    } else if (event.key === '/') {
+      calculator.chooseOperation('/')
+      calculator.updateDisplay()
+    } else if (event.key === '*') {
+      calculator.chooseOperation('*')
+      calculator.updateDisplay()
+    } else if (event.key === 'Escape') {
+      calculator.clear()
+      calculator.updateDisplay()
+    } else if (event.key === '0') {
+      calculator.appendNumber(0)
+      calculator.updateDisplay()
+    } else if (event.key === '1') {
+      calculator.appendNumber(1)
+      calculator.updateDisplay()
+    } else if (event.key === '2') {
+      calculator.appendNumber(2)
+      calculator.updateDisplay()
+    } else if (event.key === '3') {
+      calculator.appendNumber(3)
+      calculator.updateDisplay()
+    } else if (event.key === '4') {
+      calculator.appendNumber(4)
+      calculator.updateDisplay()
+    } else if (event.key === '5') {
+      calculator.appendNumber(5)
+      calculator.updateDisplay()
+    } else if (event.key === '6') {
+      calculator.appendNumber(6)
+      calculator.updateDisplay()
+    } else if (event.key === '7') {
+      calculator.appendNumber(7)
+      calculator.updateDisplay()
+    } else if (event.key === '8') {
+      calculator.appendNumber(8)
+      calculator.updateDisplay()
+    } else if (event.key === '9') {
+      calculator.appendNumber(9)
+      calculator.updateDisplay()
+    }
+  });
 });
