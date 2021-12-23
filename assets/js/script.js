0e8b24988e1068dfa796d6a02f07e08388702e10
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-23 13:03:40 +0800

    Add new variables

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Variables:
+const keysNum = document.querySelectorAll('[data-number]')
+const keysOperation = document.querySelectorAll('[data-operation]')
+const keyEqual = document.querySelector('[data-equals]')
+const keyDel = document.querySelector('[data-delete]')
+const keyReset = document.querySelector('[data-reset]')
+const prevNum = document.getElementById('previous_output')
+const curNum = document.getElementById('current_output')
